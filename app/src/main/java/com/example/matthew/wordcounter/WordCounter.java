5dd3e5f84df5dcb2368e4d7aae603e965992646a
Matthew Thorne <mwilsonthorne@gmail.com>
2018-04-24 07:47:53 +0100

    Finished MVP and refactored split method

--- app/src/main/java/com/example/matthew/wordcounter/WordCounter.java
+++ app/src/main/java/com/example/matthew/wordcounter/WordCounter.java
@@ -1,10 +1,12 @@
 package com.example.matthew.wordcounter;
 
+import java.util.ArrayList;
+
 public class WordCounter {
 
     public static int countWords(String words){
 
-        String[] word = words.split("\\s+");
+        String[] word = words.split(" ");
         return word.length;
     }
 
